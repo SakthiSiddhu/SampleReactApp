ca35dc13833bf7edbaea74c13de4b5659185b524
SakthiSiddhu <sakthisiddu01@gmail.com>
2024-10-25 09:46:17 +0530

    initialcommit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import ItemList from './ItemList';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Welcome to My Simple React App</h1>
+            <ItemList />
+        </div>
+    );
+};
+
+export default App;
---
+++ src/ItemList.jsx
@@ -0,0 +1,19 @@
+// src/ItemList.js
+import React from 'react';
+
+const ItemList = () => {
+    const items = ['Apple', 'Banana', 'Cherry', 'Date'];
+
+    return (
+        <div>
+            <h2>Fruit List</h2>
+            <ul>
+                {items.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default ItemList;
